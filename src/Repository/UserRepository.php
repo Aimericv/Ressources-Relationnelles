4b8757a675b1b076810692b36c7f653cddf38ebb
loiswera <walorise@gmail.com>
2024-02-06 21:51:57 +0100

    Help terminé

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,6 +21,16 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    public function findAdminEmails(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.roles LIKE :roles')
+            ->setParameter('roles', '["ROLE_ADMIN"]')
+            ->select('u.email')
+            ->getQuery()
+            ->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+    }
+
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
