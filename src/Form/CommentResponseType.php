5f2b5c2f62d43228b5164415f5dd2060fbcf81d5
Haddoualla Mohamedamine <mohamed.haddou_alla@etudiant.univ-lr.fr>
2024-03-12 12:33:50 +0100

    commentaire

---
+++ src/Form/CommentResponseType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\CommentResponse;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentResponseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date')
+            ->add('comment', EntityType::class, [
+                'class' => Comment::class,
+'choice_label' => 'id',
+            ])
+            ->add('commentToComment', EntityType::class, [
+                'class' => Comment::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CommentResponse::class,
+        ]);
+    }
+}
