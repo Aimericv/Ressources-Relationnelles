1982197b0dbab2ba27cf855abdaee914d7d4245e
loiswera <walorise@gmail.com>
2024-04-03 22:02:03 +0200

    fix home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,71 +27,16 @@ class HomeController extends AbstractController
         $visitDate = new \DateTime();
         $session->set('visitDates', [$visitDate->format('Y-m-d H:i:s')]);
 
-        $images = $imagesRepository->findAll();
-        $imagesPosts = [];
         $utilisateur = $this->getUser();
-
-        $posts = [];
-
-        $repostPosts = [];
 
         if ($utilisateur) {
             $follows = $utilisateur->getFollows();
-
-            $imagesPosts = [];
-
-            foreach ($follows as $follow) {
-                $followPosts = $follow->getPosts()->toArray();
-            
-                foreach ($followPosts as $post) {
-                    if ($post->getStatus()->getId() == 3) { // Vérifiez si le statut du post est égal à 3
-                        $posts[] = $post; // Ajoutez le post au tableau $posts
-            
-                        $post_id = $post->getId();
-                        $imagesPostId = [];
-                        foreach ($images as $image) {
-                            if ($post_id == $image->getPostId()->getId()) {
-                                $imagesPostId[] = $image;
-                            }
-                        }
-                        $imagesPosts[$post_id] = $imagesPostId;
-                    }
-                }
-            }
-            
-            $abonnees = $utilisateur->getFollows();
-            $imagesReposts = [];
-
-            foreach ($abonnees as $repostUser) {
-                $repostPosts = $repostUser->getReposts();
-
-                foreach ($repostPosts as $repost) {
-                    $repost_id = $repost->getId();
-                    $imagesRepostId = [];
-
-                    foreach ($images as $image) {
-                        if ($repost_id == $image->getPostId()->getId()) {
-                            $imagesRepostId = $image;
-                        }
-                    }
-                    $imagesReposts[$repost_id] = $imagesRepostId;
-                }
-            }
-
         }
         $allposts = $postRepository->findBy(['status' => 3]);
 
-        dd($imagesPosts);
-
         return $this->render('default/index.html.twig', [
-            'user' => $this->getUser(),
-            'posts' => $posts, 
-            'allposts'=>$allposts,  
-            'repostPosts' => $repostPosts,
-            'utilisateur' => $utilisateur, 
-            'images' => $imagesPosts,
-            'repostImage' => $imagesReposts,
-            'repostUser' => $repostUser,
+            'user' => $utilisateur,
+            'posts'=>$allposts,
         ]);
     }
 }
